d8b18d906873e716bdc130863e590805f7f661ee
Frédéric Yhuel <frederic.yhuel@dalibo.com>
2025-01-24 18:20:51 +0100

    evict_from_both_caches: remove call to fincore

--- fyhuel/evict_from_both_caches.sql
+++ fyhuel/evict_from_both_caches.sql
@@ -6,7 +6,7 @@ SELECT DISTINCT pg_buffercache_evict(bufferid)
 
 SELECT current_setting('data_directory') || '/' || pg_relation_filepath(:'relname') AS relpath \gset
 
-COPY (SELECT format ('fincore %s; dd oflag=nocache conv=notrunc,fdatasync count=0 of=%s',
+COPY (SELECT format ('dd oflag=nocache conv=notrunc,fdatasync count=0 of=%s',
 		     :'relpath', :'relpath')
      ) TO '/tmp/evict_index_from_page_cache.sh';
 
